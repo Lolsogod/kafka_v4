d9242e214b05e97bfea06457e0f5c7e2642d49fb
DIO <46161640+Lolsogod@users.noreply.github.com>
2023-03-28 22:06:07 +0400

    data report api

--- app2/index.js
+++ app2/index.js
@@ -90,4 +90,116 @@ app.get('/s/movies', (req, res) => __awaiter(void 0, void 0, void 0, function* (
 app.get('/s/reviews', (req, res) => __awaiter(void 0, void 0, void 0, function* () {
     res.send(yield Review.find(req.body));
 }));
+//report api
+app.get('/r/top-rating', (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    let report = yield Review.aggregate([
+        {
+            '$group': {
+                '_id': '$movie',
+                'rating': {
+                    '$avg': '$rating'
+                }
+            }
+        }, {
+            '$lookup': {
+                'from': 'movies',
+                'localField': '_id',
+                'foreignField': '_id',
+                'as': 'movie_info'
+            }
+        }, {
+            '$unwind': '$movie_info'
+        }, {
+            '$project': {
+                '_id': 1,
+                'title': '$movie_info.title',
+                'rating': 1
+            }
+        }, {
+            '$limit': 10
+        }, {
+            '$sort': {
+                'rating': -1
+            }
+        }
+    ]);
+    res.send(report);
+}));
+app.get('/r/most-popular', (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    let report = yield Review.aggregate([
+        {
+            '$group': {
+                '_id': '$movie',
+                'votes': {
+                    '$count': {}
+                }
+            }
+        }, {
+            '$lookup': {
+                'from': 'movies',
+                'localField': '_id',
+                'foreignField': '_id',
+                'as': 'movie_info'
+            }
+        }, {
+            '$unwind': '$movie_info'
+        }, {
+            '$project': {
+                '_id': 1,
+                'title': '$movie_info.title',
+                'votes': 1
+            }
+        }, {
+            '$limit': 10
+        }, {
+            '$sort': {
+                'votes': -1
+            }
+        }
+    ]);
+    res.send(report);
+}));
+app.get('/r/top-reviews/:id', (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    let report = yield Review.aggregate([
+        {
+            '$match': {
+                'movie': new mongoose_1.Types.ObjectId(req.params.id)
+            }
+        }, {
+            '$lookup': {
+                'from': 'movies',
+                'localField': 'movie',
+                'foreignField': '_id',
+                'as': 'movie_info'
+            }
+        }, {
+            '$lookup': {
+                'from': 'users',
+                'localField': 'author',
+                'foreignField': '_id',
+                'as': 'user_info'
+            }
+        }, {
+            '$unwind': '$movie_info'
+        }, {
+            '$unwind': '$user_info'
+        }, {
+            '$project': {
+                '_id': 1,
+                'movie': '$movie_info.title',
+                'author': '$user_info.login',
+                'rating': 1,
+                'text': 1,
+                'likes': 1
+            }
+        }, {
+            '$limit': 10
+        }, {
+            '$sort': {
+                'likes': -1
+            }
+        }
+    ]);
+    res.send(report);
+}));
 app.listen(process.env.PORT);
--- app2/index.ts
+++ app2/index.ts
@@ -93,5 +93,116 @@ app.get('/s/movies', async (req,res)=>{
 app.get('/s/reviews', async (req,res)=>{
     res.send(await Review.find(req.body))
 })
-
+//report api
+app.get('/r/top-rating', async (req,res)=>{
+    let report = await Review.aggregate([
+        {
+          '$group': {
+            '_id': '$movie', 
+            'rating': {
+              '$avg': '$rating'
+            }
+          }
+        }, {
+          '$lookup': {
+            'from': 'movies', 
+            'localField': '_id', 
+            'foreignField': '_id', 
+            'as': 'movie_info'
+          }
+        }, {
+          '$unwind': '$movie_info'
+        }, {
+          '$project': {
+            '_id': 1, 
+            'title': '$movie_info.title', 
+            'rating': 1
+          }
+        }, {
+          '$limit': 10
+        }, {
+          '$sort': {
+            'rating': -1
+          }
+        }
+      ])
+    res.send(report)
+})
+app.get('/r/most-popular', async (req,res)=>{
+    let report = await Review.aggregate([
+        {
+          '$group': {
+            '_id': '$movie', 
+            'votes': {
+              '$count': {}
+            }
+          }
+        }, {
+          '$lookup': {
+            'from': 'movies', 
+            'localField': '_id', 
+            'foreignField': '_id', 
+            'as': 'movie_info'
+          }
+        }, {
+          '$unwind': '$movie_info'
+        }, {
+          '$project': {
+            '_id': 1, 
+            'title': '$movie_info.title', 
+            'votes': 1
+          }
+        }, {
+          '$limit': 10
+        }, {
+          '$sort': {
+            'votes': -1
+          }
+        }
+      ])
+    res.send(report)
+})
+app.get('/r/top-reviews/:id', async (req,res)=>{
+    let report = await Review.aggregate([
+        {
+          '$match': {
+            'movie': new Types.ObjectId(req.params.id)
+          }
+        }, {
+          '$lookup': {
+            'from': 'movies', 
+            'localField': 'movie', 
+            'foreignField': '_id', 
+            'as': 'movie_info'
+          }
+        }, {
+          '$lookup': {
+            'from': 'users', 
+            'localField': 'author', 
+            'foreignField': '_id', 
+            'as': 'user_info'
+          }
+        }, {
+          '$unwind': '$movie_info'
+        }, {
+          '$unwind': '$user_info'
+        }, {
+          '$project': {
+            '_id': 1, 
+            'movie': '$movie_info.title', 
+            'author': '$user_info.login', 
+            'rating': 1, 
+            'text': 1, 
+            'likes': 1
+          }
+        }, {
+          '$limit': 10
+        }, {
+          '$sort': {
+            'likes': -1
+          }
+        }
+      ])
+    res.send(report)
+})
 app.listen(process.env.PORT)
